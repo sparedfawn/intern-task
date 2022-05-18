ce61479daa63a4053181e2074eb66069cc72dab3
Krzysztof Kalinowski <k.kalinowski2000@gmail.com>
2022-05-18 17:20:48 +0200

    completed all the tasks listed in readme.md file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
 import Dot from "./Dot";
 
 function App() {
+    const [selectedDot, setSelectedDot] = useState(-1); // index of currently selected dot, we are starting with -1 because there is no dot selected
+
+    const handleSelectingDot = (key) => {
+        setSelectedDot(key);
+    };
+
     return (
         <div className="dots-wrapper">
-            {new Array(20).fill('#000000').map((_, i) => (
-                <Dot key={i}/>
+            {new Array(20).fill("#000000").map((_, i) => (
+                <Dot key={i} index={i} selectedDot={selectedDot} handleSelectingDot={handleSelectingDot} />
             ))}
         </div>
     );
--- src/Dot.js
+++ src/Dot.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import './Dot.css';
+import React, { useState, useEffect } from "react";
+import randomColor from "randomcolor";
+import "./Dot.css";
 
-const Dot = () => {
-    // TODO: put your code here
+const Dot = ({ index, selectedDot, handleSelectingDot }) => {
+    const [backgroundColor, setBackgroundColor] = useState();
+
+    // this useEffect will be triggered every time that selected dot will change, so it can reset color to default when changing dot
+    useEffect(() => {
+        setBackgroundColor(selectedDot === index && getRandomLightColor());
+    }, [selectedDot]);
+
+    const handleClickingDot = () => {
+        handleSelectingDot(index);
+        setBackgroundColor(getRandomLightColor());
+    };
+
+    const style = {
+        backgroundColor: backgroundColor, // it is either a color or false, if false then it takes value from css file
+    };
+
+    const divContent = selectedDot === index && index; // displays index if in selected dot or nothing
+
     return (
-        <div className="dot"/>
+        <div className="dot" style={style} onClick={handleClickingDot}>
+            {divContent}
+        </div>
     );
 };
+
+function getRandomLightColor() {
+    return randomColor({ luminosity: "light" }); // luminosity is light the text can be easly read
+}
 
 export default Dot;
